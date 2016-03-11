b45d491bff7b8e2c733ddc197eccd48bd1f53acc
sreeniii <sreenivaskanthamneni@gmail.com>
2016-03-11 15:59:50 -0600

    more examples

---
+++ worldlygreeter.scala
@@ -0,0 +1,11 @@
+class WorldlyGreeter(greeting: String) {
+  def greet() = {
+    val worldlyGreeting = WorldlyGreeter.worldify(greeting)
+    println(worldlyGreeting)
+  }
+}
+
+// The WorldlyGreeter companion object
+object WorldlyGreeter {
+  def worldify(s: String) = s + ", world!"
+}
